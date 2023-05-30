25b18554b8b7fd2a5121634101eb4468bf50eec5
Lion12xD <103289468+Lion12xD@users.noreply.github.com>
2023-05-29 20:08:06 -0600

    Se agrega Scripting en Bash
    Se agrega Scripting en bash con todos los scripts utilizados en esa practica

---
+++ Scripting en Bash/bro.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+# Script bro.sh
+# 07/03/2023 – Leonardo
+#
+read -p “Escribe tu nombre:“ nombre
+echo “Hola, $nombre. Seamos amigos!”
---
+++ Scripting en Bash/netdiscover.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+# Script netdiscover.sh
+# 07/03/2023 - Leonardo
+# Escaner de red basico en BASH
+
+# Determinando el segmento de red
+if which ifconfig > /dev/null; then
+	echo "Comando ifconfig existe..."
+	direccion_ip=$(ifconfig | grep inet | grep -v "127.0.0.1" | awk '{print $2}')
+	echo "Esta es tu direccion ip: $direccion_ip"
+	subred=$(ifconfig | grep inet | grep -v "127.0.0.1" | awk '{print $2}' | awk -F. '{print $1"."$2"."$3"."}')
+	echo "Esta es tu subred: $subred"
+else
+	echo "No existe el comando ifconfig...usando ip"
+	direccion_ip=$(ip addr show | grep inet | grep -v "127.0.0.1" | awk '{print $2}')
+	echo "Esta es tu direccion ip: $direccion_ip"
+	subred=$(ip addr show | grep inet | grep -v "127.0.0.1" | awk '{print $2}' | awk -F. '{print $1"."$2"."$3"."}')
+	echo "Esta es tu subred: $subred"
+fi
+
+for ip in {1..254}; do
+	if ping -q -c 4 "${subred}${ip}" > /dev/null; then
+		echo "Host responde: ${subred}${ip}"
+	fi
+done
